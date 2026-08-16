10baf1c888db7ed48020032b9edf798f9c2b1f7c
= <=>
2026-08-16 17:36:54 +1000

    feat: camera rotates

--- entities/wish/wish.gd
+++ entities/wish/wish.gd
@@ -1,7 +1,6 @@
 extends Node3D
 
 @onready var _camera := %CameraRig as Node3D
-@onready var _camera_pivot := %CameraPivot as Node3D
 @onready var _ball := %Ball as RigidBody3D
 @onready var _state_machine := %StateMachine as StateMachine
 @onready var activating_state := %ActivatingState as State
@@ -15,7 +14,7 @@ func _physics_process(_delta):
 	if settled > 0:
 		return
 
-	var waiting_input = _handle_action()
+	_handle_action()
 
 ## Settle Ball ##
 var settled = 2.0
@@ -32,11 +31,46 @@ func _handle_camera_follow():
 	var delta = get_process_delta_time()
 	var ball_position = _ball.global_position
 	var movement_direction = _ball.linear_velocity.normalized()
-	if movement_direction.length() > 0.1:
-		var target_position = ball_position - movement_direction * 3 + Vector3(0, 2, 0)
-		_camera.global_position = _camera.global_position.lerp(target_position, delta * 3)
-	_camera.look_at(ball_position + Vector3(0, 1, 0))
 
+	if movement_direction.length() > 0.1:
+		var gravity_vector = PhysicsServer3D.area_get_param(
+			get_viewport().find_world_3d().space,
+			PhysicsServer3D.AREA_PARAM_GRAVITY_VECTOR
+		)
+
+		var gravity_dir = gravity_vector.normalized()
+		var camera_up = -gravity_dir
+		var forward = movement_direction.slide(gravity_dir)
+
+		if forward.length() > 0.01:
+			forward = forward.normalized()
+			var right = forward.cross(camera_up).normalized()
+
+			# Camera Follow
+			var target_position = (
+				ball_position
+				- forward * .5
+				+ camera_up * 1.5
+			)
+			_camera.global_position = _camera.global_position.lerp(
+				target_position,
+				delta * 10
+			)
+
+			# Camera Rotation
+			var target_basis = Basis(
+				right,
+				camera_up,
+				-forward
+			).orthonormalized()
+			var current_rotation := _camera.global_basis.get_rotation_quaternion()
+			var target_rotation := target_basis.get_rotation_quaternion()
+			var rotation_speed := 3
+			var new_rotation := current_rotation.slerp(
+				target_rotation,
+				delta * rotation_speed
+			)
+			_camera.global_basis = Basis(new_rotation)
 
 ## Ball Actions ##
 func _handle_action():
